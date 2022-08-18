bb2eeb540ad1e0b1d01ed5cd59f425735e463fdf
1sanyo <sanyo.adonai@student.moringaschool.com>
2022-08-18 10:27:21 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 function scuberGreetingForFeet(){
   // Write your code here!
-}
+  function scuberGreetingForFeet(ride){
+    if (ride <= 199) {
+      return 'This one is on me!'
+    } else if (ride <= 2001) {
+      return 'I will gladly take your thirty bucks.'
+    } else if (ride >= 2500) {
+      return 'No can do.'
+    }
+  }
 
 function ternaryCheckCity(){
   // Write your code here!
-}
-
+  function ternaryCheckCity(place){
+    return ternaryCheckCity = place === "NYC"? 'Ok, sounds good.' : 'No go.'
+   }
+  }
 function switchOnCharmFromTip(){
   // Write your code here!
-}
+  switch(tip) {
+    case 'generous': 
+    return 'Thank you so much.';
+    break;
+    case 'not as generous':
+      return 'Thank you.';
+    break;
+    default:
+      return 'Bye.';
+    break;
+    }
+  }
